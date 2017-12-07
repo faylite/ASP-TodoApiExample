60900e33c38280a119761712fe7431a1830861cc
Benjamin Vevang <benvev95@gmail.com>
2017-12-07 16:16:52 +0100

    Renamed example test to example test

---
+++ TodoApp-UnitTests/ExampleTest.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace TodoApp_UnitTests
+{
+    public class ExampleTest
+    {
+        [Fact]
+        public void TestSanity()
+        {
+            Assert.False(false);
+            Assert.True(true);
+        }
+    }
+}
